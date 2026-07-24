7ea35e074a78d0083e1b0dd70183bc5ffe2a9eb9
viect6o <phamvietthang2006hd@gmail.com>
2026-07-24 21:28:03 +0700

    Interface/Abstract and Wrapper Class design

---
+++ models/SequenceModel.py
@@ -0,0 +1,27 @@
+from torch import Tensor, nn
+
+from .base import RecurrentCell
+
+class SequenceModel(nn.Module):
+
+    def __init__(self, cell: RecurrentCell, hidden_dim: int, output_dim: int):
+        super.__init__()
+        self.cell = cell
+        self.output_layer = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Process a sequence and return final prediction
+        @params:
+            x: Input sequence, shape (batch_size, seq_len, input_dim)
+        
+        return:
+            Prediction, shape (batchsize, output_dim)
+        """
+
+        batch_size = x.size(0)
+        state = self.cell.init_state(batch_size=batch_size, device=x.device)
+        for t in range(x.size(1)):
+            h_t, state = self.cell(x[:,t,:], state)
+
+        return self.output_layer(h_t)
---
+++ models/base.py
@@ -0,0 +1,35 @@
+# ./base.py
+
+from abc import ABC, abstractmethod
+import torch 
+from torch import Tensor, nn
+
+class RecurrentCell(nn.Module):
+
+    @abstractmethod
+    def forward(self, x_t: Tensor, hidden_state: Tensor | tuple[Tensor, Tensor]) -> tuple[Tensor, Tensor]:
+        """
+        Compute one recurrent transition
+
+        @params:
+            x_t: Input at timestep t, shape (batch_size, input_dim)
+            hidden_state : Previous recurrent state
+                - RNN/GRU use h_{t-1}
+                - LSTM use (h_{t-1}, c_{t-1})
+        return:
+            h_t: next hidden_state
+            new_state: Updated recurrent state:
+                - timestep t + 1
+                - RNN/GRUL h_t
+                - LSTM: (h_t, c_t)
+        """
+
+        raise NotImplementedError
+
+    @abstractmethod
+    def init_state(self, batch_size: int, device: torch.device) -> Tensor | tuple[Tensor, Tensor]:
+        """
+        Initialize the recurrent state for a batch
+        """
+
+        raise NotImplementedError
